b17d8c3311a1e399625e08dc485248303e466eb9
Arctiqa <Sinitro@yandex.ru>
2024-01-26 11:21:24 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from src.utils import (get_vacancies, employers_list, vacancies_list, filling_database, create_tables)
 
 
+companies = {'VK': '15478', 'Сбер-IT': '3529', 'Yandex': '1740', 'Merlion': '816',
+             'Альфа-банк': '80', 'Алабуга': '68587', 'Surf-It': '5998412', 'Тинькофф': '78638',
+             'Doubletapp': '3096092', 'Почта Банк': '1049556'}
+
+
 def main():
-    vacancies = get_vacancies('python', '1740')
-    vac = vacancies_list(vacancies)
-    emp = employers_list(vacancies)
     create_tables()
-    filling_database(emp, vac, 'vacancies')
+    vacancy_name = 'python'
+
+    for company in companies.values():
+        vacancies = get_vacancies(vacancy_name, company)
+        if len(vacancies) == 0:
+            continue
+        vac = vacancies_list(vacancies)
+        emp = employers_list(vacancies)
+
+        filling_database(emp, vac)
 
 
 if __name__ == '__main__':
