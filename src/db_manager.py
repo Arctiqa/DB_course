97a5774b8e16434ad5b94e1edeef21858dbe7201
Arctiqa <Sinitro@yandex.ru>
2024-01-26 14:12:08 +0800

    update db_manager.py

--- src/db_manager.py
+++ src/db_manager.py
@@ -10,41 +10,58 @@ class DBManager:
 
     def get_companies_and_vacancies_count(self):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT company, COUNT(vacancy) FROM vacancies GROUP BY company")
+        cursor.execute("""SELECT employers.company_name, COUNT(vacancy_name) FROM employers
+                       JOIN vacancies ON employers.employer_id = vacancies.employer_id
+                       GROUP BY employers.company_name""")
         companies_and_vacancies = cursor.fetchall()
         cursor.close()
         return companies_and_vacancies
 
     def get_all_vacancies(self):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT company, vacancy, salary, link FROM vacancies")
+        cursor.execute("""
+        SELECT employers.company_name, vacancy_name, salary_from, salary_to, url 
+        FROM employers 
+        JOIN vacancies ON employers.employer_id = vacancies.employer_id""")
         all_vacancies = cursor.fetchall()
         cursor.close()
         return all_vacancies
 
     def get_avg_salary(self):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT AVG(salary) FROM vacancies")
+        cursor.execute("SELECT AVG(salary_to) FROM vacancies")
         avg_salary = cursor.fetchone()[0]
         cursor.close()
         return avg_salary
 
-    def get_vacancies_with_higher_salary(self):
-        avg_salary = self.get_avg_salary()
+    def get_avg_salary_by_employers(self):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT company, vacancy, salary, link FROM vacancies WHERE salary > %s", (avg_salary,))
+        cursor.execute("""SELECT employers.company_name, AVG(vacancies.salary_to) FROM employers
+        JOIN vacancies ON employers.employer_id = vacancies.employer_id
+        GROUP BY employers.company_name, vacancies.employer_id
+        """)
+        avg_salary = cursor.fetchall()
+        cursor.close()
+        return avg_salary
+
+    def get_vacancies_with_higher_salary(self):
+        cursor = self.conn.cursor()
+        cursor.execute("""SELECT employers.company_name, vacancy_name, salary_from, salary_to, published
+        FROM employers
+        JOIN vacancies ON employers.employer_id = vacancies.employer_id
+        WHERE salary_to > (SELECT AVG(salary_to) FROM vacancies)
+        """)
         high_salary_vacancies = cursor.fetchall()
         cursor.close()
         return high_salary_vacancies
 
     def get_vacancies_with_keyword(self, keyword):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT company, vacancy, salary, link FROM vacancies WHERE LOWER(vacancy) LIKE %s",
-                       ('%' + keyword.lower() + '%',))
+        cursor.execute(f"""SELECT employers.company_name, vacancy_name, salary_from, salary_to, published, requirements, experience
+        FROM employers
+        JOIN vacancies ON employers.employer_id = vacancies.employer_id
+        WHERE LOWER(vacancy_name) LIKE '%{keyword}%'
+        """)
         keyword_vacancies = cursor.fetchall()
         cursor.close()
         return keyword_vacancies
-
-
-hh = DBManager('localhost', 'HH_vacancies_DB',
-               'postgres', '12345')
