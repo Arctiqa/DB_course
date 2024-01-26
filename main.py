cf144dc5e99153e24e02e90ee3f4deb98488dc5d
Arctiqa <Sinitro@yandex.ru>
2024-01-26 14:12:16 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.utils import (get_vacancies, employers_list, vacancies_list, filling_database, create_tables)
-
+from src.processing import db_operations
 
 companies = {'VK': '15478', 'Сбер-IT': '3529', 'Yandex': '1740', 'Merlion': '816',
              'Альфа-банк': '80', 'Алабуга': '68587', 'Surf-It': '5998412', 'Тинькофф': '78638',
@@ -7,17 +7,34 @@ companies = {'VK': '15478', 'Сбер-IT': '3529', 'Yandex': '1740', 'Merlion': 
 
 
 def main():
-    create_tables()
-    vacancy_name = 'python'
+    print('Приветствую! Введите название базы данных, к которой хотите подключиться')
+    user_input_db = input(':')
 
-    for company in companies.values():
-        vacancies = get_vacancies(vacancy_name, company)
-        if len(vacancies) == 0:
-            continue
-        vac = vacancies_list(vacancies)
-        emp = employers_list(vacancies)
+    print('Продолжить с заполнением базы данных? (y/n)')
+    user_input = input().lower()
 
-        filling_database(emp, vac)
+    if user_input == 'y':
+        try:
+            create_tables(user_input_db)
+            print('По какому ключевому слову производить поиск вакансий?')
+            vacancy_name = input(':').lower()
+
+            print('Идет заполнение базы данных')
+            for company in companies.values():
+                vacancies = get_vacancies(vacancy_name, company)
+                if len(vacancies) == 0:
+                    continue
+                vac = vacancies_list(vacancies)
+                emp = employers_list(vacancies)
+                filling_database(emp, vac, user_input_db)
+
+                db_operations(user_input_db)
+
+        except Exception as e:
+            print(e)
+
+    elif user_input == 'n':
+        db_operations(user_input_db)
 
 
 if __name__ == '__main__':
